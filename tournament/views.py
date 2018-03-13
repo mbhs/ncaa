076bb8d3fc8e939cd9ea6479b16bf1e660ecc2b1
Noah Singer <singerng@gmail.com>
2018-03-13 08:55:33 -0400

    further updates for django 2 compatibility

--- tournament/views.py
+++ tournament/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import get_object_or_404, render, render_to_response
 from django.http import HttpResponseRedirect, HttpResponse
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.views import generic
 from django import forms
 from django.contrib.auth.decorators import login_required
